9b5a25f98d397b8089e31ba91e4dc761d5a5f0f3
Ping Wan <550322229@qq.com>
2024-04-30 11:44:16 +0200

    result for test1

--- test/2.py
+++ test/2.py
@@ -1,43 +1,30 @@
-case = None
-case_duration = None
-case_index = None
+fm = None
+aligned_traces = None
+lang_fit = None
+im = None
+net = None
+tbr_result = None
 import pm4py
-
-import pandas as pd
-
-from pm4py.objects.conversion.log import converter as log_converter
-
-from pm4py.algo.discovery.alpha import algorithm as alpha_miner
-
-from pm4py.visualization.petri_net import visualizer as pn_visualizer
-
-from pm4py.util import constants
-
-from pm4py.statistics.start_activities.log import get as start_activities
-
-from pm4py.statistics.end_activities.log import get as end_activities
-
-from datetime import timedelta
-
-import numpy as np
 import re, os, pickle, sys
 sign = sys.argv[1]
-with open(f'/home/hr546787/Code_Parser/pkl/event_log_{sign}.pkl', 'rb') as f:
-    event_log = pickle.load(f)
+with open(f'/home/hr546787/Code_Parser/pkl/log_{sign}.pkl', 'rb') as f:
+    log = pickle.load(f)
 
-with open(f'/home/hr546787/Code_Parser/pkl/case_durations_{sign}.pkl', 'rb') as f:
-    case_durations = pickle.load(f)
-
-case_index = float(sys.argv[3])
-case_index = int(case_index)
-if case_index < len(event_log):
-    case = event_log[case_index]
-    case_duration = case[-1]['time:timestamp'] - case[0]['time:timestamp']
-    case_durations.append(case_duration)
-    print(f"Case {case.attributes['concept:name']} duration: {case_duration}")
-with open(f'/home/hr546787/Code_Parser/pkl/case_{sign}.pkl', 'wb') as f:
-    pickle.dump(case, f)
-with open(f'/home/hr546787/Code_Parser/pkl/case_duration_{sign}.pkl', 'wb') as f:
-    pickle.dump(case_duration, f)
-with open(f'/home/hr546787/Code_Parser/pkl/case_index_{sign}.pkl', 'wb') as f:
-    pickle.dump(case_index, f)
+noise_threshold = float(sys.argv[3])
+net, im, fm = pm4py.discover_petri_net_inductive(log, noise_threshold=
+    noise_threshold)
+tbr_result = pm4py.conformance_diagnostics_token_based_replay(log, net, im, fm)
+aligned_traces = pm4py.conformance_diagnostics_alignments(log, net, im, fm)
+lang_fit = pm4py.fitness_token_based_replay(log, net, im, fm)
+with open(f'/home/hr546787/Code_Parser/pkl/fm_{sign}.pkl', 'wb') as f:
+    pickle.dump(fm, f)
+with open(f'/home/hr546787/Code_Parser/pkl/aligned_traces_{sign}.pkl', 'wb') as f:
+    pickle.dump(aligned_traces, f)
+with open(f'/home/hr546787/Code_Parser/pkl/lang_fit_{sign}.pkl', 'wb') as f:
+    pickle.dump(lang_fit, f)
+with open(f'/home/hr546787/Code_Parser/pkl/im_{sign}.pkl', 'wb') as f:
+    pickle.dump(im, f)
+with open(f'/home/hr546787/Code_Parser/pkl/net_{sign}.pkl', 'wb') as f:
+    pickle.dump(net, f)
+with open(f'/home/hr546787/Code_Parser/pkl/tbr_result_{sign}.pkl', 'wb') as f:
+    pickle.dump(tbr_result, f)
--- test/4.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/local_rwth/bin/zsh
-#SBATCH --job-name=4
-srun /rwthfs/rz/cluster/home/hr546787/Code_Parser/new_env/bin/python /home/hr546787/Code_Parser/test/4.py $1 $2 $3
-sacct -j $SLURM_JOB_ID --format=JobID,Start,End,Elapsed > /home/hr546787/Code_Parser/results/test3/$1_${SLURM_JOB_NAME}$2.log
